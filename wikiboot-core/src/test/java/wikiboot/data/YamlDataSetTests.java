a15f246c80f43ee180070237ef8ac42f456c2ff6
bep <bjorn.erik.pedersen@gmail.com>
2014-09-03 17:56:56 +0200

    Add more test cases for YamlDataSet

--- wikiboot-core/src/test/java/wikiboot/data/YamlDataSetTests.java
+++ wikiboot-core/src/test/java/wikiboot/data/YamlDataSetTests.java
@@ -1,6 +1,7 @@
 package wikiboot.data;
 
 import org.junit.Test;
+import org.springframework.core.io.ByteArrayResource;
 import wikiboot.DataItem;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -20,5 +21,45 @@ public class YamlDataSetTests {
             assertThat(item.getModel().get("name"), is(notNullValue()));
         }
     }
+
+    @Test
+    public void textLiteral() {
+        ByteArrayResource resource = new ByteArrayResource("test".getBytes());
+        YamlDataSet dataSet = new YamlDataSet(resource);
+
+        assertThat(dataSet.getDataItems().size(), is(1));
+
+        DataItem dataItem = dataSet.getDataItems().get(0);
+
+        assertThat(dataItem.getModel().size(), is(1));
+        assertThat(dataItem.getModel().get("document"), is("test"));
+    }
+
+    @Test
+    public void nonStringKey() {
+        ByteArrayResource resource = new ByteArrayResource("1: value".getBytes());
+        YamlDataSet dataSet = new YamlDataSet(resource);
+
+        assertThat(dataSet.getDataItems().size(), is(1));
+
+        DataItem dataItem = dataSet.getDataItems().get(0);
+
+        assertThat(dataItem.getModel().size(), is(1));
+        assertThat(dataItem.getModel().get("[1]"), is("value"));
+    }
+
+    @Test
+    public void emptyDocument() {
+        ByteArrayResource resource = new ByteArrayResource("---".getBytes());
+        YamlDataSet dataSet = new YamlDataSet(resource);
+        assertThat(dataSet.getDataItems().size(), is(0));
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void dataSetNotFound() {
+        new YamlDataSet("doesNotExist.yaml");
+
+    }
+
 
 }
